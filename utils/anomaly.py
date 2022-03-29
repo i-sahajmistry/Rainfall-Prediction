81447927c6a5a4179ff45e70d069aa1ccca4b30e
i-sahajmistry <sahajmistry005@gmail.com>
2022-03-29 18:19:40 +0530

    UWND10 issue ressolved

--- utils/anomaly.py
+++ utils/anomaly.py
@@ -4,13 +4,13 @@ import pandas as pd
 import glob
 import os
 
-mydir = 'D:/Sahaj/Raw_data/Regrid/uwnd/*'
+mydir = 'D:/Sahaj/Raw_data/Regrid/uwnd/uwnd10.csv'
 
-for i, file in enumerate(glob.glob(mydir)):
-    df = pd.read_csv(file, header=[0,1], index_col=0)
-    data = np.array(df)
-    mean = np.mean(data[12:42], axis=0)
-    data = data - mean
-    df = pd.DataFrame(data, index=df.index)
-    df.to_csv(f'D:/Sahaj/Anomaly/uwnd/uwnd{i+1}.csv')
+# for i, file in enumerate(glob.glob(mydir)):
+df = pd.read_csv(mydir, header=[0,1], index_col=0)
+data = np.array(df)
+mean = np.mean(data[12:42], axis=0)
+data = data - mean
+df = pd.DataFrame(data, index=df.index)
+df.to_csv(f'D:/Sahaj/uwnd{10}.csv')
     
